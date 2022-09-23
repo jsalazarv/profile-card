59a0be24cb0166d8773a88dc090e7d0aed7c65b0
Juan Salazar <jsalazarv8@gmail.com>
2022-09-23 09:40:36 -0500

    Content is centered within the Avatar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Avatar } from './components/Avatar';
 function App() {
   return (
     <div className="App">
-      <Avatar type="rounded" size="large">
+      <Avatar type="circle" size="large">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGMsJNRv8en8a8mnoT_W9D7CjqV55SAPTurg&usqp=CAU"
           alt=""
