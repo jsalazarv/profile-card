c0d7d17111dd29b3a2462692f64f2fe7ca4a1fd8
jsalazarv <jsalazarv8@gmail.com>
2022-11-15 23:43:18 -0600

    Set coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,16 @@
 module.exports = {
+    rootDir: ".",
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{jx,jsx,ts,tsx}',
+        '!**/node_modules/**',
+        '!<rootDir>/__test__/**',
+    ],
+    coverageThreshold: {
+        global: {
+            lines: 75,
+        },
+    },
     testEnvironment: 'jest-environment-jsdom',
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
